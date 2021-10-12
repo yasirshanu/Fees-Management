08202631e209bd01f5d9eef1984506bf56e6398f
Yasir Ansari <yasirshanu@gmail.com>
2021-10-12 22:47:53 +0530

    Version 0.6.7 (Beta)
    Small footer fix.

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
 <footer class="main-footer text-sm">
     <strong>Copyright &copy; <?php echo date("Y", time()); ?> <b><a href="#" class="text-black"><?php echo $sitename; ?></a></b></strong>
-    All rights reserved.
+    All rights reserved. <b>Developed by <a href="https://www.yasiransari.in" target="_blank">Yasir Ansari.</a></b>
     <div class="float-right d-none d-sm-inline-block">
-        <b>Version</b> 0.6.6 (Beta)
+        <b>Version</b> 0.6.7 (Beta)
     </div>
 </footer>
